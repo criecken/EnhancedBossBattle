cc2b4f2570e709815e4bfbe63d3247af7da927e6
criecken <31356063+criecken@users.noreply.github.com>
2017-11-06 22:16:00 -0700

    Update StartTVC.swift
    Adding comments

--- EnhancedBossBattle/StartTVC.swift
+++ EnhancedBossBattle/StartTVC.swift
@@ -9,40 +9,40 @@
 import UIKit
 
 class StartTVC: UITableViewController, CharDelegate, BattleDelegate {
-    var options = [String()]
-    var bossWeapon = String()
-    var heroWeapon = String()
-    var bossHP : Int = Int()
-    var heroHP : Int = Int()
-    var selectedRow = ""
-    var delegate : CharDelegate?
+    var options = [String()]    //stores the options passed from the App Delegate
+    var bossWeapon = String()   //stores boss Weapon
+    var heroWeapon = String()   //stores hero Weapon
+    var bossHP : Int = Int()    //stores boss hp
+    var heroHP : Int = Int()    //stores hero hp
+    var selectedRow = ""        //placeholder for row
+    var delegate : CharDelegate?    //delegate for character update
     
-    var battleDelegate : BattleDelegate?
-    var dest = ""
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    var record = [Battle]()
+    var battleDelegate : BattleDelegate?    //delegate for battle update
+    var dest = ""                           //stores destination from Start
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext   //save context
+    var record = [Battle]()                 //initialize previous record 
     
     func update(selectedRow: String, initHP: Int, weapon: String) {
         if selectedRow == "Hero"{
-            let hero = Brawlers(initHP: initHP, finalHP: initHP, weapon: weapon)
-            heroWeapon = hero.weapon
-            heroHP = hero.initHP
-            print (hero.finalHP)
+            let hero = Brawlers(initHP: initHP, finalHP: initHP, weapon: weapon)    //init hero
+            heroWeapon = hero.weapon    //set hero weapon   
+            heroHP = hero.initHP        //set hero hp
+            print (hero.finalHP)        //check 
         }
         else if selectedRow == "Boss"{
-            let boss = Brawlers(initHP: initHP, finalHP: initHP, weapon: weapon)
-            bossWeapon = boss.weapon
-            bossHP = boss.initHP
+            let boss = Brawlers(initHP: initHP, finalHP: initHP, weapon: weapon)    //init boss 
+            bossWeapon = boss.weapon    //set boss weapon
+            bossHP = boss.initHP        //set boss hp
         }
     }
     
     func updateRecord(winner: String, heroInitHp: Int, bossInitHp: Int, winnerHP: Int, heroWeapon: String, bossWeapon: String) {
-     getData()
+     getData()                          //get data 
     }
     
     
     @IBAction func recordButton(_ sender: Any) {
-        dest = "record"
+        dest = "record"     //tell app to go to record view 
        // performSegue(withIdentifier: "recordSegue", sender: self)
     }
     
@@ -50,9 +50,9 @@ class StartTVC: UITableViewController, CharDelegate, BattleDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getData()
-        let startButton = UIBarButtonItem(title: "Start", style: UIBarButtonItemStyle.plain, target: self, action: #selector(self.start(sender:)))
-        self.navigationItem.rightBarButtonItem = startButton
+        getData()       //get data
+        let startButton = UIBarButtonItem(title: "Start", style: UIBarButtonItemStyle.plain, target: self, action: #selector(self.start(sender:)))  //create start button
+        self.navigationItem.rightBarButtonItem = startButton    //init start button 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -60,16 +60,16 @@ class StartTVC: UITableViewController, CharDelegate, BattleDelegate {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
-    func getData() {
+    func getData() {        // get data function 
         do {
-            record = try context.fetch(Battle.fetchRequest()) 
+            record = try context.fetch(Battle.fetchRequest()) //update record
         } catch {
-            print("Fetching Failed")
+            print("Fetching Failed")                        // check
         }
     }
     
     @objc func start(sender: UIBarButtonItem) {
-         dest = "start"
+         dest = "start"          //transition to battle vc 
         performSegue(withIdentifier: "startSelected", sender: self)
         // delegateUpdate?.update(newEmails: changedEmails, currentEmails: emails, updateRow: selectedRow)
     }
@@ -92,14 +92,14 @@ class StartTVC: UITableViewController, CharDelegate, BattleDelegate {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return options.count
+        return options.count        //return num options 
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "menuCell", for: indexPath)
         // Configure the cell...
-        cell.textLabel?.text = options[indexPath.row]
+        cell.textLabel?.text = options[indexPath.row] //sets label text
         return cell
     }
     
@@ -107,19 +107,13 @@ class StartTVC: UITableViewController, CharDelegate, BattleDelegate {
         //TODO: react to user selecting row
         //I want the detail view controller to update based on the row that I selected
         
-        print("In didSelectRowAt")
+        print("In didSelectRowAt")      // check 
         //TODO: get cell information
-        selectedRow = options[indexPath.row]
+        selectedRow = options[indexPath.row]    //hero or boss
         //call segue manually
-        if selectedRow == "Record"{
-            dest = "record"
-            performSegue(withIdentifier: "recordSegue", sender: self)
-        }
-        else {
-            dest = "setup"
-            performSegue(withIdentifier: "cellSelected", sender: self)
-        }
-        
+
+         dest = "setup" //sets destination for setup tvc
+         performSegue(withIdentifier: "cellSelected", sender: self) // perform segue
         
     }
     /*
@@ -165,7 +159,7 @@ class StartTVC: UITableViewController, CharDelegate, BattleDelegate {
         
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-        if dest == "start" {
+        if dest == "start" {    // passes battle parameters
             let destVC = segue.destination as! BattleVC
             destVC.bossHP = self.bossHP
             destVC.heroHP = self.heroHP
@@ -175,7 +169,7 @@ class StartTVC: UITableViewController, CharDelegate, BattleDelegate {
             destVC.initHeroHP = self.heroHP
             destVC.battleDelegate = self.battleDelegate
         }
-        else if dest == "setup"{
+        else if dest == "setup"{        // passes setup parameters
         let destVC = segue.destination as! SetupTVC
         destVC.selectedRow = selectedRow
         destVC.delegateUpdate = delegate
@@ -188,7 +182,7 @@ class StartTVC: UITableViewController, CharDelegate, BattleDelegate {
             destVC.options = ["No"]
         }
         }
-        else{
+        else{   //sets destination to record 
             let destVC = segue.destination as! RecordTVC
         }
         print("here")
